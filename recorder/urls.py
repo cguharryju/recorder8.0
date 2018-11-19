6dab52e7bdaa3661e953d79ff50bf8e7b340fa4e
cguharryju <42321463+cguharryju@users.noreply.github.com>
2018-11-19 15:56:34 +0800

    Add files via upload

--- recorder/urls.py
+++ recorder/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import  include, url
-from trips.views import recorder, model_form_upload, upload, name_post, set_name, upload_word, model_form_uploadRecog,uploadRecog,recog,recognizer,main,upload_file,self_recorder,self_name_post
+from trips.views import recorder, model_form_upload, upload, name_post, set_name, upload_word, model_form_uploadRecog,uploadRecog,recog,recognizer,main,upload_file,self_recorder,self_name_post,name_post2
 
 urlpatterns = {
     path('admin/', admin.site.urls),
@@ -29,7 +29,7 @@ urlpatterns = {
     path('uploadRecog', model_form_upload),
     path('uploadRecog_server', uploadRecog),
     path('recogResult', recog),
-    path('recognizer', recognizer),
+    path('recognizer', name_post2),
     path('upload_file',upload_file),
     path('self_recorder',self_name_post),
     path('',main),
